145d40ee478ad4ba039b94ccb7ac28aa5ec4d6d3
leodaher <leonardo.daher@usp.br>
2020-03-28 20:13:39 -0300

    Add initial app screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:toodue_mobile/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Display login, signup and logo text', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Login'), findsOneWidget);
+    expect(find.text('Sign up'), findsOneWidget);
+    expect(find.text('TooDue'), findsOneWidget);
   });
 }
